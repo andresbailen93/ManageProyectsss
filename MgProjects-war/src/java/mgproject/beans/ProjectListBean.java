75f05c9e47946efd6a92a38455a450da9c023199
Jesus <jjesusmp@gmaiLl.com>
2016-03-04 10:20:45 +0100

    [NEW] Añadido ProjectListBean

---
+++ MgProjects-war/src/java/mgproject/beans/ProjectListBean.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mgproject.beans;
+
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import mgproject.ejb.ProjectFacade;
+import mgproject.entities.Project;
+import mgproject.entities.Users;
+
+/**
+ *
+ * @author alejandroruiz
+ */
+@ManagedBean
+@RequestScoped
+public class ProjectListBean {
+
+    @EJB
+    private ProjectFacade projectFacade;
+
+    private List<Project> list;
+
+    public List<Project> getList() {
+        return list;
+    }
+
+    public void setList(List<Project> list) {
+        this.list = list;
+    }
+    
+    @PostConstruct
+    public void init(){
+        
+        Users u = new Users("1","alex");
+        list = projectFacade.findByUser(u);
+        
+    }
+    
+
+    /**
+     * Creates a new instance of projectListBean
+     */
+    public ProjectListBean() {
+    }
+    
+}
