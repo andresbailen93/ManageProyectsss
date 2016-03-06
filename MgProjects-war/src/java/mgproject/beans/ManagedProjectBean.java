e5a1cfbccb9ad4147e62a2e3a84bfc425ba47f51
Jesus <jjesusmp@gmaiLl.com>
2016-03-06 11:55:41 +0100

    [NEW] Añadido eliminar proyecto.

--- MgProjects-war/src/java/mgproject/beans/ManagedProjectBean.java
+++ MgProjects-war/src/java/mgproject/beans/ManagedProjectBean.java
@@ -5,9 +5,16 @@
  */
 package mgproject.beans;
 
+import java.util.Collection;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import mgproject.ejb.ProjectFacade;
+import mgproject.ejb.TaskFacade;
+import mgproject.ejb.UsersFacade;
+import mgproject.entities.Project;
+import mgproject.entities.Task;
 
 /**
  *
@@ -19,6 +26,12 @@ public class ManagedProjectBean {
     
     @ManagedProperty(value="#{loginBean}")
     private LoginBean loginBean;
+    @EJB
+    private TaskFacade taskFacade;
+    @EJB
+    private ProjectFacade projectFacade;
+    @EJB
+    private UsersFacade usersFacade;
     
     /**
      * Creates a new instance of ManagedProjectBean
@@ -32,5 +45,34 @@ public class ManagedProjectBean {
 
     public void setLoginBean(LoginBean loginBean) {
         this.loginBean = loginBean;
+    }
+
+    public TaskFacade getTaskFacade() {
+        return taskFacade;
+    }
+
+    public void setTaskFacade(TaskFacade taskFacade) {
+        this.taskFacade = taskFacade;
+    }
+
+    public ProjectFacade getProjectFacade() {
+        return projectFacade;
+    }
+
+    public void setProjectFacade(ProjectFacade projectFacade) {
+        this.projectFacade = projectFacade;
+    }
+    
+    public String doDeleteProject(Project project){
+        Collection<Task> tasks = project.getTaskCollection();
+        System.out.println(project.getName());
+        
+        for (Task task : tasks) {
+            taskFacade.remove(task);
+        }  
+        projectFacade.remove(project);
+        loginBean.setProject_list(projectFacade.findByUser(usersFacade.find(loginBean.getIdUser())));
+        
+        return ("profile");
     }
 }
